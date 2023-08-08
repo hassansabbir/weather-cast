d055b8f88886343b6267bc4bc154482e5d4feea4
Mahbubnishan <mahbubnishan6@gmail.com>
2023-08-09 00:04:49 +0600

    add somethinhg

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ export default function Home() {
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <p>Hello</p>
+          <h2> this is new thing </h2>
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
