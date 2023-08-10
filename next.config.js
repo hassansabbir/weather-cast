1047a60b1f1e6c9285e4ec4c23357cc94b313c2a
Mithila Khan <mithilakhan082@gmail.com>
2023-08-10 19:17:16 +0600

    banner

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ibb.co",
-      },
-    ],
-  },
+  
 };
 
 module.exports = nextConfig;
