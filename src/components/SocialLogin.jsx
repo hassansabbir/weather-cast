753bdb312f09ec9428161063f1749191232ff0f6
Mahmud Hasan Sabbir <121848364+hassansabbir@users.noreply.github.com>
2023-08-17 19:42:48 +0600

    Revert "commit with authentication"

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,29 +1,13 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import app from "@/Firebase/Firebase.config";
 
 const SocialLogin = () => {
-      const auth=getAuth(app);
-      const provider =new GoogleAuthProvider();
-   
-      const handleGoogleSignIn=()=>{
-        signInWithPopup(auth,provider)
-        .then(result=>{
-          const user=result.user;
-          console.log(user);
-        })
-        .catch(error=>{
-          console.log(error)
-        })
-      }
-
   return (
     <>
       <div className="divider">OR</div>
       <div className="space-y-5">
-        <button onClick={handleGoogleSignIn}
+        <button
           type="button"
           className="btn btn-outline text-xl hover:border-none hover:bg-sky-400 w-full"
         >
