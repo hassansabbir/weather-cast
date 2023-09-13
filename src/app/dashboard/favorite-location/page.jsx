3976f2958fa3328ebf62a8e76296b11ddfd1b84f
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-09-13 16:17:10 +0600

    Update code added

--- src/app/dashboard/favorite-location/page.jsx
+++ src/app/dashboard/favorite-location/page.jsx
@@ -8,34 +8,21 @@ const favoriteLocationPage = () => {
   const { user } = useContext(AuthContext)
   const [favLocations, setFavLocations] = useState(null);
   // const apiKey = '41a5c84ae7ccfff1bc9491b25aa4dbde'; 
-<<<<<<< HEAD
 
   useEffect(() => {
-    fetch(`https://weather-cast-server.vercel.app/favLoc?email=${user?.email}`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        setFavLocations(data);
-      })
-    // console.log(favLocation); 
-  }, [user?.email])
-=======
-  
-  useEffect(()=>{
     if (user?.email) {
       fetch(`https://weather-cast-server.vercel.app/favLoc/${user.email}`)
-      .then(res =>res.json())
-      .then(data=>{
-        console.log(data);
-        setFavLocations(data);
-      }) 
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          setFavLocations(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
     }
     // console.log(favLocation); 
-  } ,[user?.email])
->>>>>>> 73e3652e05a7f6c97b8ea4b5a97ac017f7044dd5
+  }, [user?.email])
 
   return (
     <div>
