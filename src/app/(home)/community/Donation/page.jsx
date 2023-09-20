b2b49cd6372d1ec27e6f5893a959afcf31b8d66a
Mahbubnishan <mahbubnishan6@gmail.com>
2023-09-20 17:47:40 +0600

    commit with polishing part2

--- src/app/(home)/community/Donation/page.jsx
+++ src/app/(home)/community/Donation/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import donation from '../../../../assets/donation.jpg'
+import donation2 from '../../../../assets/donation2.svg'
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -11,9 +12,9 @@ const DonationPage = () => {
                <div>
                <h1 className='text-5xl font-bold my-6 text-start'>Bring a smile to <br /> Their faces.</h1>
                 <h3 className='text-2xl text-start'>A new way of giving back to your <br /> loved charities.</h3>
-                <p className='text-xl text-start my-6'>Lorem ipsum, dolor sit amet <br /> consectetur adipisicing elit. <br />Corporis, reiciendis!</p>
+                <p className='text-xl text-start my-6 font-semibold'><span className=' text-2xl text-fuchsia-600'>Happiness </span> doesn’t result <br /> from what we get,<br /> but from what we <span className=' text-2xl text-red-700'>give....</span> </p>
                 <Link href='/'>
-                <button className='btn btn-neutral text-start'>Explore our project</button>
+                <button className='btn btn-accent text-start'>----Explore our project</button>
                 </Link>
                </div>
                 </div>
@@ -25,14 +26,18 @@ const DonationPage = () => {
                 <div>
                 <p className='text-5xl my-6 text-slate-50'>Would You like to <span className=' text-yellow-500 font-semibold'>Help</span>  <br /> <span className=' text-amber-500 font-semibold'>people</span>  Across the Globe?</p>
                 <h3 className=' text-xl text-slate-50'> Data from January 1 through November 30, 2018</h3>
+                <div className='flex  items-center my-8 gap-3 '>
+                    <Image alt='' src={donation2}></Image>
+                    <h3 className=' text-slate-50 text-xl'>If you want to change the <br /> world, one wish at a time,<span className=' text-lime-400 font-bold'> help <br /> kids</span></h3>
                 </div>
-                <div className=' border-zinc-300'>
+                </div>
+                <div className=' border-zinc-30 bg-orange-100 rounded-lg p-20'>
                 <div className='flex flex-col gap-10'>
                     <input type="number" 
                   placeholder="amount of money"
                   className="input input-bordered p-5 "
                   name="name"/>
-                    <button className='btn px-8 pt-6 text-2xl bg-emerald-300'>Donate Now</button>
+                    <button className='btn px-8 pt-6 pb-6 text-2xl bg-emerald-300'>Donate Now</button>
                 </div>
                 </div>
             </div>
